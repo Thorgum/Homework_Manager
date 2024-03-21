a8b8655a9d0638721c3f8ad8394fe1295787aeea
Thorgum <redmann@macstudent.com.au>
2024-03-21 12:13:16 +1100

    I have now gotten the correct days remaining but am figuring out how to access it in other functions

--- app.js
+++ app.js
@@ -4,23 +4,14 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("loadClicked:", loadClicked);
 
     // Clear local storage if Load button wasn't clicked
-    if (!loadClicked || loadClicked !== 'true') {
+    if (!loadClicked || loadClicked !== 'false') {
         console.log("Clearing local storage...");
         localStorage.clear();
     }
 });
-
-function loadData() {
-    // Set flag indicating Load button was clicked
-    localStorage.setItem('loadClicked', 'true');
-
-    // Your existing code...
-}
-
 function addMainTask() {
     var mainTask = document.getElementById('mainTask').value;
     var dueDate = document.getElementById('dueDate').value;
-
     // Debugging
     console.log("Adding Main Task:", mainTask);
     console.log("Due Date:", dueDate);
@@ -33,9 +24,24 @@ function addMainTask() {
     // Save main task to local storage
     localStorage.setItem('mainTask_' + (mainTasksCount - 1), mainTask);
     localStorage.setItem('dueDate_' + (mainTasksCount - 1), dueDate);
-
+    countdown(dueDate)
     // Call displayMainTask to show the newly added main task
     displayMainTask(mainTask, dueDate, mainTasksCount - 1);
+}
+
+function countdown(duedate) {
+    // Get the current date
+    today = new Date();
+    console.log(today)
+// Calculate the difference in days between today and Christmas
+    var one_day = 1000 * 60 * 60 * 24;
+    var due = new Date(duedate[0]+duedate[1]+duedate[2]+duedate[3],duedate[5]+duedate[6]-1,duedate[8]+duedate[9])
+// Log the number of days left until Christmas to the console
+    console.log(due)
+    var countdown = console.log(Math.ceil((  due.getTime() - today.getTime()) / (one_day)) +
+        " days left");
+    localStorage.setItem('countdown',countdown)
+
 }
 
 function addSubTask(taskIndex) {
@@ -59,13 +65,13 @@ function addSubTask(taskIndex) {
     document.getElementById('subTaskDate_' + taskIndex).value = '';
 }
 
-function displayMainTask(mainTask, dueDate, taskIndex) {
+function displayMainTask(mainTask, dueDate, taskIndex,countdown) {
     var output = document.getElementById('output');
-
+    console.log(countdown)
     // Create HTML for main task
     var taskContainer = `
         <div class="taskContainer">
-            <div class="mainTaskOutput">Main Task: ${mainTask}, Due Date: ${dueDate}</div>
+            <div class="mainTaskOutput">Main Task: ${mainTask}, Due Date: ${countdown}</div>
             
             <!-- Subtask section -->
             <div id="subTaskSection_${taskIndex}" class="subTaskSection">
